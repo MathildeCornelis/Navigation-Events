518ea3498837b8b89741a27852295235988ff868
MathildeCornelis <mathilde.cornelis26@gmail.com>
2022-12-05 12:19:53 +0100

    navigate

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+/*Select the last child of the <ol> tag and put it at the beginning of the list*/
 const ol = document.querySelector('ol');
-const lastli = ol.children[3];
-ol.appendChild(lastli);
+const a = ol.children[0]
+const b = ol.children[4];
+ol.insertBefore(b, a);
+
+/*Move the <h2> of the third section in the second one and vice-versa*/
+const secondh2 = document.querySelectorAll('h2')[1];
+const thirdh2 = document.querySelectorAll('h2')[2];
+const secondsection = document.querySelectorAll('section')[1];
+
+secondsection.insertBefore(thirdh2, secondh2);
+
+const div = document.querySelector('div');
+const parag = div.lastElementChild;
+
+div.insertBefore(secondh2, parag);
+
+/*Delete the last section from the DOM, we don't need it anyways*/
+const thirdsection = document.querySelectorAll('section')[2];
+thirdsection.remove();
